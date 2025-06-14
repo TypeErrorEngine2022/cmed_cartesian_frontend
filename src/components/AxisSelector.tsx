0558f46959edb675f21e7d56a762ac3857e55b4c
Jacky <jackychen924@gmail.com>
2025-06-15 01:42:58 +0800

    feat: integrate form into axis selector

--- src/components/AxisSelector.tsx
+++ src/components/AxisSelector.tsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 import { AxisConfigRecord, CartesianPlaneConfig } from "../types";
+import { Form, Select } from "antd";
 
 interface AxisSelectorProps {
   record: AxisConfigRecord;
   dimensions: string[];
   onSettingsChange: (settings: CartesianPlaneConfig) => void;
+  form?: any; // Optional form instance that can be passed from parent
+  formItemClassName?: string; // Optional class for styling form items
 }
 
 export const AxisSelector: React.FC<AxisSelectorProps> = ({
   record,
   dimensions,
   onSettingsChange,
+  form: externalForm,
+  formItemClassName = "",
 }) => {
+  const [form] = Form.useForm(externalForm);
   const [localSettings, setLocalSettings] = useState<CartesianPlaneConfig>(
     record.settings
   );
@@ -19,7 +25,13 @@ export const AxisSelector: React.FC<AxisSelectorProps> = ({
   // Update local state when record changes from parent
   useEffect(() => {
     setLocalSettings(record.settings);
-  }, [record.settings]);
+    form.setFieldsValue({
+      yPositive: record.settings.yPositive.name,
+      xPositive: record.settings.xPositive.name,
+      yNegative: record.settings.yNegative.name,
+      xNegative: record.settings.xNegative.name,
+    });
+  }, [record.settings, form]);
 
   const handleSettingChange = (
     axisType: keyof CartesianPlaneConfig,
@@ -44,78 +56,98 @@ export const AxisSelector: React.FC<AxisSelectorProps> = ({
     onSettingsChange(updatedSettings);
   };
 
+  const handleFormValuesChange = (_: any, allValues: any) => {
+    // Update settings when form values change
+    if (allValues.yPositive)
+      handleSettingChange("yPositive", allValues.yPositive);
+    if (allValues.xPositive)
+      handleSettingChange("xPositive", allValues.xPositive);
+    if (allValues.yNegative)
+      handleSettingChange("yNegative", allValues.yNegative);
+    if (allValues.xNegative)
+      handleSettingChange("xNegative", allValues.xNegative);
+  };
+
   return (
     <div className="m-6">
-      <div className="relative w-[50%] lg:w-[20%] aspect-square flex items-center justify-center">
-        {/* Vertical line */}
-        <div className="absolute w-0.5 h-full bg-gray-300"></div>
+      <Form
+        form={form}
+        initialValues={{
+          yPositive: localSettings?.yPositive.name,
+          xPositive: localSettings?.xPositive.name,
+          yNegative: localSettings?.yNegative.name,
+          xNegative: localSettings?.xNegative.name,
+        }}
+        onValuesChange={handleFormValuesChange}
+        layout="vertical"
+      >
+        <div className="relative w-[30%] lg:w-[20%] aspect-square flex items-center justify-center">
+          {/* Vertical line */}
+          <div className="absolute w-0.5 h-full bg-gray-300"></div>
 
-        {/* Horizontal line */}
-        <div className="absolute h-0.5 w-full bg-gray-300"></div>
+          {/* Horizontal line */}
+          <div className="absolute h-0.5 w-full bg-gray-300"></div>
 
-        {/* Y+ (Up) Axis */}
-        <div className="absolute top-0">
-          <select
-            className="p-4 pr-8 border rounded bg-white shadow-md"
-            value={localSettings?.yPositive.name}
-            onChange={(e) => handleSettingChange("yPositive", e.target.value)}
-          >
-            {dimensions?.map((column) => (
-              <option key={`yPos-${column}`} value={column}>
-                {column}
-              </option>
-            ))}
-          </select>
+          {/* Y+ (Up) Axis */}
+          <div className="absolute top-0 transform -translate-y-1/2">
+            <Form.Item name="yPositive" className={formItemClassName} noStyle>
+              <Select
+                className="w-full border rounded bg-white shadow-md"
+                popupMatchSelectWidth={false}
+                options={dimensions?.map((column) => ({
+                  label: column,
+                  value: column,
+                }))}
+              />
+            </Form.Item>
+          </div>
+
+          {/* X+ (Right) Axis */}
+          <div className="absolute right-0 transform translate-x-1/2">
+            <Form.Item name="xPositive" className={formItemClassName} noStyle>
+              <Select
+                className="w-full border rounded bg-white shadow-md"
+                popupMatchSelectWidth={false}
+                options={dimensions?.map((column) => ({
+                  label: column,
+                  value: column,
+                }))}
+              />
+            </Form.Item>
+          </div>
+
+          {/* Y- (Down) Axis */}
+          <div className="absolute bottom-0 transform translate-y-1/2">
+            <Form.Item name="yNegative" className={formItemClassName} noStyle>
+              <Select
+                className="w-full border rounded bg-white shadow-md"
+                popupMatchSelectWidth={false}
+                options={dimensions?.map((column) => ({
+                  label: column,
+                  value: column,
+                }))}
+              />
+            </Form.Item>
+          </div>
+
+          {/* X- (Left) Axis */}
+          <div className="absolute left-0 transform -translate-x-1/2">
+            <Form.Item name="xNegative" className={formItemClassName} noStyle>
+              <Select
+                className="w-full border rounded bg-white shadow-md"
+                popupMatchSelectWidth={false}
+                options={dimensions?.map((column) => ({
+                  label: column,
+                  value: column,
+                }))}
+              />
+            </Form.Item>
+          </div>
+
+          {/* Center point */}
+          <div className="absolute w-3 h-3 bg-gray-800 rounded-full"></div>
         </div>
-
-        {/* X+ (Right) Axis */}
-        <div className="absolute right-0">
-          <select
-            className="p-4 pr-8 border rounded bg-white shadow-md"
-            value={localSettings?.xPositive.name}
-            onChange={(e) => handleSettingChange("xPositive", e.target.value)}
-          >
-            {dimensions?.map((column) => (
-              <option key={`xPos-${column}`} value={column}>
-                {column}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {/* Y- (Down) Axis */}
-        <div className="absolute bottom-0">
-          <select
-            className="p-4 pr-8 border rounded bg-white shadow-md"
-            value={localSettings?.yNegative.name}
-            onChange={(e) => handleSettingChange("yNegative", e.target.value)}
-          >
-            {dimensions?.map((column) => (
-              <option key={`yNeg-${column}`} value={column}>
-                {column}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {/* X- (Left) Axis */}
-        <div className="absolute left-0">
-          <select
-            className="p-4 pr-8 border rounded bg-white shadow-md"
-            value={localSettings?.xNegative.name}
-            onChange={(e) => handleSettingChange("xNegative", e.target.value)}
-          >
-            {dimensions?.map((column) => (
-              <option key={`xNeg-${column}`} value={column}>
-                {column}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {/* Center point */}
-        <div className="absolute w-3 h-3 bg-gray-800 rounded-full"></div>
-      </div>
+      </Form>
     </div>
   );
 };
